eeace78b084781fbae0c8ae1a4d59dc287647e2d
Bavos <87132712+Bavos@users.noreply.github.com>
2026-05-01 23:59:36 -0300

    fix: align Root component with required remotion bootstrap shape

---
+++ remotion-iamazing/src/Root.tsx
@@ -0,0 +1,15 @@
+import {Composition} from 'remotion';
+import {Main} from './Composition';
+
+export const Root = () => {
+  return (
+    <Composition
+      id="IamazingSchool"
+      component={Main}
+      durationInFrames={600}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  );
+};
